4d5e376bb480e4017ddd5c9a1056f6d120c6a846
AlekUp24 <alekp24@gmail.com>
2024-10-25 10:14:11 +0200

    commit

---
+++ PortfolioWebApp/Components/Pages/Weather.razor.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+using static PortfolioWebApp.Components.Widgets.Jokes;
+
+namespace PortfolioWebApp.Components.Pages
+
+{
+    public partial class Weather
+    {
+        private WeatherData? response;
+        private LocationData? location;
+        private string iconUrl;
+        private bool firstLoad;
+
+        // to HIDE later
+        private string API_KEY = "a48e893f5b18eec63e166b52def1e3b0";
+
+
+        [SupplyParameterFromForm]
+        protected string cityName { get;   set; }
+        [SupplyParameterFromForm]
+        protected string stateCode { get; set; }
+        [SupplyParameterFromForm]
+        protected string countryCode { get; set; }
+
+
+
+        protected async override Task OnInitializedAsync()
+        {
+            firstLoad = true;
+        }
+
+        public class WeatherData
+        {
+            public MainData Main { get; set; }
+            public List<WeatherDescription> Weather { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class MainData
+        {
+            public decimal Temp { get; set; }
+        }
+
+        public class WeatherDescription
+        {
+            public string Main { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+        }
+
+        public class LocationData
+        {
+            public decimal Lat { get; set; }
+            public decimal Lon { get; set; }
+        }
+
+        public async Task GetCurrWeather(decimal lat, decimal lon)
+        {
+            response = await Http.GetFromJsonAsync<WeatherData>($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric");
+            iconUrl = "https://openweathermap.org/img/wn/" + response.Weather[0].Icon + "@2x.png";
+        }
+
+        public async Task GetLanLon()
+        {
+            response = null;
+            firstLoad = false;
+            var locationDataList = await Http.GetFromJsonAsync<List<LocationData>>($"http://api.openweathermap.org/geo/1.0/direct?q={cityName},{stateCode},{countryCode}&appid={API_KEY}");
+            location = locationDataList?.FirstOrDefault();
+            
+            await GetCurrWeather(location.Lat, location.Lon);
+        }
+    }
+}
+
