2513279ec32ddffd1f0cbc7dae8a92cc452d3f18
AlekUp24 <alekp24@gmail.com>
2024-12-01 17:46:57 +0100

    CQRS and MediatR implemented

--- PortfolioWebApp.Domain/Entities/Ideas/InnovationIdeasEntity.cs
+++ PortfolioWebApp.Domain/Entities/Ideas/InnovationIdeasEntity.cs
@@ -3,13 +3,7 @@
     public class InnovationIdeasEntity
     {
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(50, ErrorMessage = "Name is too long. (must be <50)")]
         public string Name { get; set; }
-
-        [Required]
-        [StringLength(150, ErrorMessage = "Description is too long. (must be <150)")]
         public string Description { get; set; }
         public IdeaCategoryEntity Category { get; set; }
         public bool Implemented { get; set; }
